d5853e55515a47a016c0cc9728aeba1ff78cae59
sadhaco <sadhaco@protonmail.com>
2022-04-09 17:11:33 +0800

    quickSort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -1,19 +1,26 @@
-function quickSort(arr, low, high) {
-  if (low < high) {
-    let pivotpos = partition(arr, low, high);
-    quickSort(arr, low, pivotpos - 1);
-    quickSort(arr, pivotpos + 1, high);
+var quickSort = (function () {
+  function quickSort(arr, low, high) {
+    if (low < high) {
+      let pivotpos = partition(arr, low, high);
+      quickSort(arr, low, pivotpos - 1);
+      quickSort(arr, pivotpos + 1, high);
+    }
   }
-}
+  function partition(arr, low, high) {
+    let pivot = arr[low];
+    while (low < high) {
+      while (low < high && arr[high] >= pivot) --high;
+      arr[low] = arr[high];
+      while (low < high && arr[low] <= pivot) ++low;
+      arr[high] = arr[low];
+    }
+    arr[low] = pivot;
+    return low;
+  }
 
-function partition(arr, low, high) {
-  let pivot = arr[low];
-  while (low < high) {
-    while (low < high && arr[high] >= pivot) --high;
-    arr[low] = arr[high];
-    while (low < high && arr[low] <= pivot) ++low;
-    arr[high] = arr[low];
-  }
-  arr[low] = pivot;
-  return low;
-}
+  return function (arr) {
+    let len = arr.length;
+    quickSort(arr, 0, len - 1);
+    return arr;
+  };
+})();
